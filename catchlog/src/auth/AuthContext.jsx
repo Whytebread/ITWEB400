748ef40d7e35fc9a340a205a71c2219f73ceaa9a
Whytebread <lukeweisbrot@gmail.com>
2025-07-17 11:57:42 -0500

    created authcontext and wrapped it in index

---
+++ catchlog/src/auth/AuthContext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+
+    // Load user from localStorage when app starts
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    // Stores user in state and localstorage
+    const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+    };
+
+    //Clears user from memory and localstorage
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+
